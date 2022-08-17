5ba0071a90ab6f0c58da559eb2f8fb9629ba1921
zerooo <kdyfutu@naver.com>
2022-08-17 19:33:21 +0900

    main page

--- src/dooribun/urls.py
+++ src/dooribun/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views as accounts_views
+from main import views
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('chat/', include('chat.urls')),
     path('login/', accounts_views.login, name='login'),
-    path('logout/', accounts_views.logout, name='logout'),
+    path('logout/', accounts_views.logout, name='logout')
 ]
