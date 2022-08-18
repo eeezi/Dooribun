e0ae9cf9c887460efe8538beeae04c5b6016b923
eeezi <alloy0035@hanmail.net>
2022-08-18 10:28:25 +0900

    post update

--- src/main/models.py
+++ src/main/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Post(models.Model):
-    area = models.CharField(max_length=100, default='노원구')
+
+    AREA_CHOICES = {
+        ('강남구', '강남구'), ('강동구', '강동구'), ('강북구', '강북구'), ('강서구', '강서구'), ('관악구', '관악구'), ('광진구', '광진구'), ('구로구', '구로구'), ('금천구', '금천구'), ('노원구', '노원구'), ('도봉구', '도봉구'), ('동대문구', '동대문구'), ('동작구', '동작구'), 
+        ('마포구', '마포구'), ('서대문구', '서대문구'), ('서초구', '서초구'), ('성동구', '성동구'), ('성북구', '성북구'), ('송파구', '송파구'), ('양천구', '양천구'), ('영등포구', '영등포구'), ('은평구', '은평구'), ('종로구', '종로구'), ('중구', '중구'), ('중랑구', '중랑구'), 
+    }
+
+    area = models.CharField(max_length=100, choices=AREA_CHOICES, default='노원구')
     location = models.CharField(max_length=100)
     item = models.CharField(max_length=100)
     price = models.IntegerField()
