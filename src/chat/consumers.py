defdfc3ee7a239d345301fc1d1070ef71a504889
eeezi <alloy0035@hanmail.net>
2022-08-19 20:04:39 +0900

    chat & wrong_login

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -4,7 +4,7 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_name = self.scope['url_route']['kwargs']['post_id']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
